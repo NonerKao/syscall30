212eb2dafb7ae40283a6f3d1678e419ee8b5bb12
Quey-Liang Kao <s101062801@m101.nthu.edu.tw>
2016-12-07 14:42:10 +0800

    code for open

---
+++ 4-open/open.c
@@ -0,0 +1,12 @@
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<stdio.h>
+
+int main(){
+	int fd = open("/home/noner/test", O_RDWR | O_TRUNC | O_CREAT, 0777);
+	printf("openning file descriptor %d\n", fd);
+	write(fd, "Hello World!\n", 13);
+	return 0;
+}
